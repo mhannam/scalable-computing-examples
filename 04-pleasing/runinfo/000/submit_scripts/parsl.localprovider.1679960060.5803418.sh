635fa345e2b8a7bfc8d3a7dfdac18e82c8eacebe
mhannam <mhannam@uw.edu>
2023-03-31 09:49:04 -0700

    trying to get rid of csv

---
+++ 04-pleasing/runinfo/000/submit_scripts/parsl.localprovider.1679960060.5803418.sh
@@ -0,0 +1,35 @@
+workon scomp
+export JOBNAME=$parsl.localprovider.1679960060.5803418
+set -e
+export CORES=$(getconf _NPROCESSORS_ONLN)
+[[ "1" == "1" ]] && echo "Found cores : $CORES"
+WORKERCOUNT=1
+FAILONANY=0
+PIDS=""
+
+CMD() {
+process_worker_pool.py  --max_workers=15 -a 127.0.0.1,128.111.85.28,included-crab -p 0 -c 1.0 -m None --poll 10 --task_port=54907 --result_port=54799 --logdir=/home/hannam/scalable-computing-examples/04-pleasing/runinfo/000/HighThroughputExecutor --block_id=1 --hb_period=30  --hb_threshold=120 --cpu-affinity none --available-accelerators  --start-method fork
+}
+for COUNT in $(seq 1 1 $WORKERCOUNT); do
+    [[ "1" == "1" ]] && echo "Launching worker: $COUNT"
+    CMD $COUNT &
+    PIDS="$PIDS $!"
+done
+
+ALLFAILED=1
+ANYFAILED=0
+for PID in $PIDS ; do
+    wait $PID
+    if [ "$?" != "0" ]; then
+        ANYFAILED=1
+    else
+        ALLFAILED=0
+    fi
+done
+
+[[ "1" == "1" ]] && echo "All workers done"
+if [ "$FAILONANY" == "1" ]; then
+    exit $ANYFAILED
+else
+    exit $ALLFAILED
+fi
---
+++ python-intro.py
@@ -0,0 +1,14 @@
+x = 4
+print(x)
+
+dict = {
+    'name' : ['Jeanette', 'Matt'],
+    'location' : ['Tucson', 'Juneau']
+}
+
+for name in dict['name']:
+    print(name)
+
+
+
+
